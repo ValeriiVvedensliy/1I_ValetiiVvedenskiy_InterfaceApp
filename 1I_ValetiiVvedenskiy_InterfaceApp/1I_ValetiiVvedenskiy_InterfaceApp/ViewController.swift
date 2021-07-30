503cbb3056e68a0bdb9289eacf2bfca8d9c191b6
Valerii <valeronmaestro@gmail.com>
2021-07-30 18:06:41 +0300

    setUp

---
+++ 1I_ValetiiVvedenskiy_InterfaceApp/1I_ValetiiVvedenskiy_InterfaceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1I_ValetiiVvedenskiy_InterfaceApp
+//
+//  Created by Valera Vvedenskiy on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
