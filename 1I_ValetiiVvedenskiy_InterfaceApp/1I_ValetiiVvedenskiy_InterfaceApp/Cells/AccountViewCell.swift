17e8b645a25303dd0eda833cdd89017c2ce4044f
Valerii <valeronmaestro@gmail.com>
2021-08-04 22:49:04 +0300

    add shadow

--- 1I_ValetiiVvedenskiy_InterfaceApp/1I_ValetiiVvedenskiy_InterfaceApp/Cells/AccountViewCell.swift
+++ 1I_ValetiiVvedenskiy_InterfaceApp/1I_ValetiiVvedenskiy_InterfaceApp/Cells/AccountViewCell.swift
@@ -4,18 +4,34 @@ class AccountViewCell: UITableViewCell {
 
     @IBOutlet weak var accountImage: UIImageView!
     @IBOutlet weak var accountText: UILabel!
+    @IBOutlet weak var shadowView: UIView!
     
     public static var Key: String = "AccountViewCell"
     static var Nib: UINib = UINib.init(nibName: "AccountViewCell", bundle: Bundle(for:  AccountViewCell.self))
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        contentView.backgroundColor = UIColor.lightGray
-        accountText.textColor = UIColor.white
+        setupShadowView()
+        setupView()
     }
     
     public func setUpCell(_ accountImage: String, _ accountName: String) {
         self.accountImage.image = UIImage(named: accountImage)
         self.accountText.text = accountName
+    }
+    
+    private func setupView() {
+        contentView.backgroundColor = UIColor.lightGray
+        accountText.textColor = UIColor.white
+        accountImage.layer.cornerRadius = accountImage.frame.height / 2
+    }
+    
+    private func setupShadowView() {
+        shadowView.layer.cornerRadius = shadowView.frame.height / 2
+        shadowView.backgroundColor = UIColor.black
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOffset = .zero
+        shadowView.layer.shadowRadius = 7
+        shadowView.layer.shadowOpacity = 1
     }
 }
