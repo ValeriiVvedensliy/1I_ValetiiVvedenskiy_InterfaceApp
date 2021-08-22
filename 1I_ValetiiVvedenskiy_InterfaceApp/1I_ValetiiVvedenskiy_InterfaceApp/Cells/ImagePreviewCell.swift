e44b017852bdfe2d44e6e067338c49b5887d01bd
Valerii <valeronmaestro@gmail.com>
2021-08-22 18:23:19 +0300

    add animation_path_two

---
+++ 1I_ValetiiVvedenskiy_InterfaceApp/1I_ValetiiVvedenskiy_InterfaceApp/Cells/ImagePreviewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ImagePreviewCell.swift
+//  1I_ValetiiVvedenskiy_InterfaceApp
+//
+//  Created by Valera Vvedenskiy on 20.08.2021.
+//
+import UIKit
+import Foundation
+
+class ImagePreviewCell: UICollectionViewCell, UIScrollViewDelegate {
+    
+    var imgView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imgView = UIImageView()
+        imgView.image = UIImage()
+        imgView.contentMode = .scaleAspectFit
+        self.addSubview(imgView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imgView.frame = self.bounds
+    }
+}
